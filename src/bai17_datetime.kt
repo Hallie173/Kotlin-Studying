5a47f68a28cdd4c27e55dc98ddd5f592069e6ba8
g15 5511 <phuonghaleduy@gmail.com>
2024-08-27 12:18:13 +0700

    Initial commit

--- src/bai17_datetime.kt
+++ src/bai17_datetime.kt
@@ -1,6 +1,24 @@
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun main(args: Array<String>) {
     val cal = Calendar.getInstance()
     println(cal.get(Calendar.DAY_OF_MONTH))
+
+    cal.set(Calendar.DAY_OF_MONTH,17)
+    cal.set(Calendar.YEAR,2004)
+    cal.set(Calendar.MONTH,3)
+
+    var DOB = cal.get(Calendar.DAY_OF_MONTH)
+    var YOB = cal.get(Calendar.YEAR)
+    var MOB = cal.get(Calendar.MONTH)
+    println("$DOB/$MOB/$YOB")
+
+    var date = cal.time
+    var format = SimpleDateFormat("dd/MM/yyyy")
+    println(format.format(date))
+
+    var format2 = SimpleDateFormat("dd/MM/yyyy hh:mm:ss a")
+    println(format2.format(date))
+
 }
