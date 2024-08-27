29b5c5782c94a1d03a1880daebdd5871ce1b1dbf
g15 5511 <phuonghaleduy@gmail.com>
2024-08-27 18:39:47 +0700

    Initial commit

---
+++ src/bai19_char.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //method
+    println('8'.isDigit())
+    println('a'.isLetter())
+    println(' '.isWhitespace())
+    println('h'.isLowerCase())
+    println('H'.isUpperCase())
+}
